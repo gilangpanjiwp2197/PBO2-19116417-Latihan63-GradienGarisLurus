5507a30bbf238437312a9257168da3cd33d17b65
gilangpanjiwp2197 <73225699+gilangpanjiwp2197@users.noreply.github.com>
2020-11-29 03:07:56 +0700

    Add files via upload

---
+++ PBO2-19116417-Latihan63-GradienGarisLurus/src/pbo2/pkg19116417/latihan63/gradiengarislurus/GarisLurus.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg19116417.latihan63.gradiengarislurus;
+
+/**
+ *
+ * @author Akmal
+ */
+public interface GarisLurus {
+    
+     public int hitungGradien(); 
+}
---
+++ PBO2-19116417-Latihan63-GradienGarisLurus/src/pbo2/pkg19116417/latihan63/gradiengarislurus/PBO219116417Latihan63GradienGarisLurus.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg19116417.latihan63.gradiengarislurus;
+
+/**
+ *
+ * @author 
+ *  NAMA        : GILANG PANJI WANENG PATI
+ *  KELAS       : PBO2
+ *  NIM         : 10116417
+ *  DESKRIPSI   : BERISI TENTANG PROGRAM MENGHITUNG GRADIEN
+ */
+public class PBO219116417Latihan63GradienGarisLurus {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+         Koordinat kdt1 = new Koordinat(2, 10, 5, 7);
+        Koordinat kdt2 = new Koordinat(5, 1, 3, 12);
+
+        System.out.println("Garis yang melalui titik (2,10) dan titik (5,7) memiliki gradien sebesar "
+                + kdt1.hitungGradien());
+        System.out.println("Garis yang melalui titik (5,1) dan titik (3,12) memiliki gradien sebesar "
+                + kdt2.hitungGradien());
+    }
+    
+}
